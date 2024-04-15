215e541dbd3a5189713fe50c9e306c096f8fc16d
bilelaa <biilelbenhlel@icloud.com>
2024-04-15 14:51:34 +0200

    network

---
+++ public/script.js
@@ -0,0 +1,37 @@
+function generateWeatherHTML(data) {
+    console.log(data)
+    const weather = data.weather[0];
+    const iconName = weather.icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${iconName}.png`;
+
+    return `
+        <h2>Weather in ${data.name}, ${data.sys.country}</h2>
+        <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
+        <p><strong>Feels like:</strong> ${data.main.feels_like} °C</p>
+        <p><strong>Description:</strong> ${weather.description}</p>
+        <img class="weather-icon" src="${iconUrl}" alt="${weather.main}">
+        <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+        <p><strong>Pressure:</strong> ${data.main.pressure} hPa</p>
+        <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+        <p><strong>Sunrise:</strong> ${new Date(
+            data.sys.sunrise * 1000
+        ).toLocaleTimeString()}</p>
+        <p><strong>Sunset:</strong> ${new Date(
+            data.sys.sunset * 1000
+        ).toLocaleTimeString()}</p>
+    `;
+}
+
+async function getWeatherData() {
+    document.getElementById("get").addEventListener("click", async function (e) {
+        const city = document.getElementById("city").value;
+        fetch(`/get-weather/${city}`)
+            .then((res) => res.json())
+            .then((data) => {
+                document.getElementById("weather-details").innerHTML = generateWeatherHTML(data);
+            })
+            .catch((err) => console.log(err));
+    });
+}
+
+document.addEventListener("DOMContentLoaded", getWeatherData);
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+console.log(process.env.API_KEY,'1')
+require("dotenv").config();
+console.log(process.env.API_KEY,'2')
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html")
+});
+
+
+
+app.get("/get-weather/:city", async (req, res) => {
+  try {
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&units=metric&APPID=${process.env.API_KEY}`;
+    const response = await fetch(weatherURL);
+    const weatherData = await response.json();
+    res.status(200).json(weatherData);
+  } catch (error) {
+    console.log("Error fetching weather data:", error);
+    res.status(500).json(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("runing on port 3000");
+});
